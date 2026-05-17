7d85a913f042df4e7a34ca7818ddc4a0ea054428
Janvi Chauhan <cjanvi720@gmail.com>
2026-05-17 23:41:23 +0530

    Create Letter combination of a phone number.cpp
    Backtracking solution for Letter Combinations of a Phone Number.

---
+++ Letter combination of a phone number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> result;
+    
+    void backtrack(string digits, int index, string current, vector<string>& mapping) {
+        // basecase
+        if(index == digits.size()) {
+        result.push_back(current);
+        return;
+    }
+
+    string letters = mapping[digits[index] - '0'];
+
+    for(char c : letters) {
+        backtrack(digits, index + 1, current + c, mapping);
+    }      
+}
+
+vector<string> letterCombinations(string digits){
+    if(digits.empty()) return {};
+
+    vector<string> mapping = {
+        "", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+    };
+
+    backtrack(digits, 0, "", mapping);
+    return result;
+    }
+};
