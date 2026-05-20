23ac4c83669417bd75f2caea8ba40450a615f8f6
Janvi Chauhan <cjanvi720@gmail.com>
2026-05-20 19:20:45 +0530

    Create Generate paranthesis.cpp
    Generate all valid parentheses using backtracking

---
+++ Generate paranthesis.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> result;
+
+    void backtrack(string current, int open, int close, int n) {
+        //base case
+        if(current.length() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+        //add'('
+        if(open < n) {
+            backtrack(current + '(', open + 1, close, n);
+        }
+
+        //add')'
+        if(close < open) {
+            backtrack(current + ')', open, close + 1, n);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        backtrack("", 0, 0, n);
+        return result;
+    }        
+    
+};
