09db500428eed14844947113cf6b817f7877a053
Janvi Chauhan <cjanvi720@gmail.com>
2026-05-30 22:41:04 +0530

    Create Missing positive.cpp
    Added cyclic sort approach for finding first missing positive integer.

---
+++ Missing positive.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+
+        //step1. place elements at correct index
+        for(int i = 0; i < n; i++) {
+            while(nums[i] > 0 && nums[i] <= n  &&
+            nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        //step2. find first missing
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != i + 1)
+            return i + 1;
+        }
+        return n + 1;
+    }
+};
