68c1cb843c5d78b45e93e5c643cc2c65d2b58c21
Janvi Chauhan <cjanvi720@gmail.com>
2026-05-31 22:16:19 +0530

    Create string multiplication.cpp
    Implemented string multiplication without using BigInteger.

---
+++ string multiplication.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int n = num1.size();
+        int m = num2.size();
+
+        vector<int> result(n + m, 0);
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+
+                int sum = mul + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10; //unit digit
+                result[i + j] += sum / 10;  //carry
+            }
+        }
+        //convert to string
+        string ans = "";
+        for(int x : result) {
+            if(!(ans.empty() && x == 0)) {
+                ans += to_string(x);
+            }
+        }
+        return ans.empty() ? "0" : ans;
+    }
+};
