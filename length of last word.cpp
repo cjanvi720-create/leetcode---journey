a37087f0b83077d2abee855efc31377c163f200c
Janvi Chauhan <cjanvi720@gmail.com>
2026-06-08 22:54:23 +0530

    Create length of last word.cpp
    Solve Length of Last Word using reverse traversal.

---
+++ length of last word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+        int length = 0;
+
+        //step 1. skip trailing space
+        while(i >= 0 && s[i] == ' ')
+            i--;
+
+            //step 2. count last word 
+            while(i >= 0 && s[i] != ' ') {
+                length++;
+                i--;
+            }
+        return length;
+    }
+};
