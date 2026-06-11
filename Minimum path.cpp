b32a24cbb106b816534ac636208ed79885a88f99
Janvi Chauhan <cjanvi720@gmail.com>
2026-06-11 22:52:37 +0530

    Create Minimum path.cpp
    Add bottom-up dynamic programming approach.

---
+++ Minimum path.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size(); // Fixed: grid[0].size() for columns
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                if (i == 0) grid[i][j] += grid[i][j - 1]; // Only move right
+                else if (j == 0) grid[i][j] += grid[i - 1][j]; // Only move down
+                else grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]); // Min of top or left
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+};
