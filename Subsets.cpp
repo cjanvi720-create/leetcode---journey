9e88794e1f4b86f70d85dbb7d37d0f3d16f1dd50
Janvi Chauhan <cjanvi720@gmail.com>
2026-07-05 22:41:32 +0530

    Create Subsets.cpp
    Add recursive solution for power set generation.

---
+++ Subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        backtrack(0, nums, curr, ans);
+        return ans;
+    }
+    void backtrack(int i, vector<int>& nums, vector<int>& curr, vector<vector<int>>& ans) {
+        if (i == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+        // include nums[i]
+        curr.push_back(nums[i]);
+        backtrack(i + 1, nums, curr, ans);
+
+        curr.pop_back();
+        backtrack(i + 1, nums, curr, ans);
+    }
+};
