107a1996b795ce3af1b34345bdb5752b43dfd155
Janvi Chauhan <cjanvi720@gmail.com>
2026-07-11 20:07:44 +0530

    Create Gray code.cpp
    Implement Gray Code using bitwise operations.

---
+++ Gray code.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res(1 << n);
+        for (int i = 0; i < (1 << n); ++i) 
+        res[i] = i ^ (i >> 1);
+        return res;
+    }
+};
