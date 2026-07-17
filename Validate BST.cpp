89f9fd964dd3bda5eb058491e3f12697ec018281
Janvi Chauhan <cjanvi720@gmail.com>
2026-07-17 22:24:37 +0530

    Create Validate BST.cpp
    Implement BST validation using recursion and range checks.

---
+++ Validate BST.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return dfs(root, LLONG_MIN, LLONG_MAX);
+    }
+    bool dfs(TreeNode* node, long long minVal, long long maxVal) {
+        if(!node) return true;
+        if(node->val <= minVal || node->val >= maxVal) return false;
+        return dfs(node->left, minVal, node->val) && dfs(node->right, node->val, maxVal);
+    }
+};
