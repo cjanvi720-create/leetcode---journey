311c816b020142d23bfc1cef0a2075fd99c771a1
Janvi Chauhan <cjanvi720@gmail.com>
2026-07-17 22:22:48 +0530

    Create Interleaving string.cpp
     Interleaving String using Space Optimized DP.

---
+++ Interleaving string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.size(), m = s2.size();
+        if (n + m!= s3.size()) return false; // ye line sabse imp
+
+        vector<bool> dp(m + 1, false);
+        dp[0] = true;
+
+        for (int j = 1; j <= m; j++)
+            dp[j] = dp[j-1] && s2[j-1] == s3[j-1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[0] = dp[0] && s1[i-1] == s3[i-1]; // s2 empty case
+            for (int j = 1; j <= m; j++) {
+                dp[j] = (dp[j] && s1[i-1] == s3[i+j-1]) ||
+                        (dp[j-1] && s2[j-1] == s3[i+j-1]);
+            }
+        }
+        return dp[m];
+    }
+};
